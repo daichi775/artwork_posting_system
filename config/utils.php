4d9dd7f2eb928945447ff1907a444b7c660e9624
D.Nagasumi <31396588+daichi775@users.noreply.github.com>
2021-04-22 14:13:37 +0900

    文言修正
    実績公開用に、システムタイトル等を修正

--- config/utils.php
+++ config/utils.php
@@ -4,9 +4,9 @@ return [
     /**
      * 募集中の合同誌企画の名前。.envで定義
      */
-    'project_name' => env('PROJECT_NAME',"合同誌企画"),
+    'project_name' => env('PROJECT_NAME',"合作本企画"),
 
-    'app_name' => env('APP_NAME',"合同誌あっぷろーだ"),
+    'app_name' => env('APP_NAME',"イラスト提出システム"),
 
     /**
      * IFTTT関係の環境変数
@@ -40,9 +40,5 @@ return [
      */
     'dpi' => 350,
 
-    /**
-     *  現在のマジカルミライの最新年度コード
-     *  cf: https://docs.google.com/document/d/17rUled0Hddvcado4DCmctYY4qjuKfrICHCCnAcY3tvA/edit?usp=sharing
-     */
     'allYearSumCode' => 255,
 ];
